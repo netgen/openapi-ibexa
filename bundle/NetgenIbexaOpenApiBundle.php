89009e0714a77a27aa8ae96269e6d7805fdd6a79
Edi Modrić <edi.modric@gmail.com>
2024-09-19 11:07:14 +0200

    Add bundle skeleton

---
+++ bundle/NetgenIbexaOpenApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaOpenApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class NetgenIbexaOpenApiBundle extends Bundle {}
