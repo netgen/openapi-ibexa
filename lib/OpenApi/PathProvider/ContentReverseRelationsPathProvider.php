050d96df4d3ddcadcdf8fa9b2b76c08edd5395e1
Edi Modrić <edi.modric@gmail.com>
2024-12-13 09:17:40 +0100

    Implement endpoint for fetching content reverse relations

---
+++ lib/OpenApi/PathProvider/ContentReverseRelationsPathProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\OpenApiIbexa\OpenApi\PathProvider;
+
+use Netgen\OpenApi\Model\MediaType;
+use Netgen\OpenApi\Model\Operation;
+use Netgen\OpenApi\Model\Parameter;
+use Netgen\OpenApi\Model\ParameterIn;
+use Netgen\OpenApi\Model\Path;
+use Netgen\OpenApi\Model\Response;
+use Netgen\OpenApi\Model\Responses;
+use Netgen\OpenApi\Model\Schema;
+use Netgen\OpenApiIbexa\OpenApi\PathProviderInterface;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\Routing\RouterInterface;
+
+final class ContentReverseRelationsPathProvider implements PathProviderInterface
+{
+    public function __construct(
+        private RouterInterface $router,
+    ) {}
+
+    public function providePaths(): iterable
+    {
+        $getOperation = new Operation(
+            [
+                new Parameter('contentId', ParameterIn::Path, new Schema\IntegerSchema()),
+                new Parameter('fieldIdentifier', ParameterIn::Path, new Schema\StringSchema()),
+                new Parameter('maxPerPage', ParameterIn::Path, new Schema\IntegerSchema()),
+                new Parameter('currentPage', ParameterIn::Path, new Schema\IntegerSchema()),
+            ],
+            null,
+            new Responses(
+                [
+                    SymfonyResponse::HTTP_OK => new Response(
+                        'Content reverse relations',
+                        [],
+                        [
+                            'application/json' => new MediaType(new Schema\ReferenceSchema('SiteApi.ContentList')),
+                        ],
+                    ),
+                ],
+            ),
+        );
+
+        $routePath = $this->router->getRouteCollection()->get('netgen_openapi_ibexa_content_reverse_relations')?->getPath() ?? '';
+
+        yield $routePath => new Path($getOperation);
+    }
+}
