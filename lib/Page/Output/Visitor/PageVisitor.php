bdec39a99b1459386c9c0476685df6a89de192b8
Edi Modrić <edi.modric@gmail.com>
2025-01-17 14:08:44 +0100

    Do not output disabled Page elements

--- lib/Page/Output/Visitor/PageVisitor.php
+++ lib/Page/Output/Visitor/PageVisitor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Netgen\OpenApiIbexa\Page\Output\Visitor;
 
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Netgen\OpenApiIbexa\Page\Output\OutputVisitor;
 use Netgen\OpenApiIbexa\Page\Output\VisitorInterface;
 use Netgen\OpenApiIbexa\Page\Page;
@@ -15,6 +16,10 @@ use function is_object;
  */
 final class PageVisitor implements VisitorInterface
 {
+    public function __construct(
+        private ConfigResolverInterface $configResolver,
+    ) {}
+
     public function accept(object $value): bool
     {
         return $value instanceof Page;
@@ -25,15 +30,18 @@ final class PageVisitor implements VisitorInterface
      */
     public function visit(object $value, OutputVisitor $outputVisitor, array $parameters = []): iterable
     {
-        if ($value->getContent() !== null) {
+        /** @var array<string, array{reference_name: string, required: bool, enabled: bool}> $pageSchemaConfig */
+        $pageSchemaConfig = $this->configResolver->getParameter('page_schema', 'netgen_open_api_ibexa');
+
+        if ($pageSchemaConfig['content']['enabled'] && $value->getContent() !== null) {
             yield 'content' => $outputVisitor->visit($value->getContent());
         }
 
-        if ($value->getLocation() !== null) {
+        if ($pageSchemaConfig['location']['enabled'] && $value->getLocation() !== null) {
             yield 'location' => $outputVisitor->visit($value->getLocation());
         }
 
-        if ($value->getLayout() !== null) {
+        if ($pageSchemaConfig['layout']['enabled'] && $value->getLayout() !== null) {
             yield 'layout' => $outputVisitor->visit($value->getLayout());
         }
 
