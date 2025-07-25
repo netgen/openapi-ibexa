42bcc77036aec98b211cdd2eb4d7006e51fee6e2
Jakov Knezovic <jknezovicc@gmail.com>
2025-07-25 10:43:31 +0200

    PROSTORIA-534 add matrix field schema provider

---
+++ lib/OpenApi/SchemaProvider/Ibexa/FieldValue/MatrixFieldValueSchemaProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\OpenApiIbexa\OpenApi\SchemaProvider\Ibexa\FieldValue;
+
+use Netgen\OpenApi\Model\Schema;
+
+final class MatrixFieldValueSchemaProvider implements FieldValueSchemaProviderInterface
+{
+    public function provideFieldValueSchema(): Schema
+    {
+        return new Schema\ArraySchema(
+            new Schema\ObjectSchema(
+                properties: [
+                    'type' => new Schema\StringSchema(),
+                    'value' => new Schema\StringSchema(),
+                ],
+                required: ['type', 'value'],
+            ),
+        );
+    }
+}
